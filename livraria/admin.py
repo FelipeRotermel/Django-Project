4c6655a770e99404191ceafec6706c407f2857c6
Felipe Rotermel <feliperotermell@gmail.com>
2023-03-17 14:50:41 -0300

    criação "Autor" e "Livros" no models.py

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from django.contrib import admin
+from livraria.models import Autor, Categoria, Editora, Livro
 
-from .models import Categoria, Editora
-
+admin.site.register(Autor)
 admin.site.register(Categoria)
-
 admin.site.register(Editora)
+admin.site.register(Livro)
