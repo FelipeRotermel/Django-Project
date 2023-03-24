b8e7c2304c5db991eb91da781609c9395669565e
Felipe Rotermel <feliperotermell@gmail.com>
2023-03-24 14:35:51 -0300

    Criação de "serializers"  e uso do API REST

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "livraria",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- livraria/models.py
+++ livraria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
@@ -27,12 +28,14 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True, validators=[
+        RegexValidator(r'^\d*\.?\d+$', 'Apenas números são permitidos.')
+    ])
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
     )
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
-    autores = models.ManyToManyField(Autor, related_name="livros", null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
