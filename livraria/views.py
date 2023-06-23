6c713eab8930d13ca4cf735d4a2463e7e867257f
Felipe Rotermel <feliperotermell@gmail.com>
2023-06-23 14:22:00 -0300

    Permission add in "views.py"

--- livraria/views.py
+++ livraria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from livraria.models import Categoria, Editora, Autor, Livro
 from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
@@ -8,17 +9,21 @@ from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSe
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+    permission_classes = [IsAuthenticated]
 
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    permission_classes = [IsAuthenticated]
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == "list":
