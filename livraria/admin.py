cebb0ca4dd1578626153985528023a6eec442244
Felipe Rotermel <feliperotermell@gmail.com>
2023-08-11 15:02:46 -0300

    TabularInline add in admin.py

--- livraria/admin.py
+++ livraria/admin.py
@@ -6,5 +6,10 @@ admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
-admin.site.register(Compra)
-admin.site.register(ItensCompra)
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
