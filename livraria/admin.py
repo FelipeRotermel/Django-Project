a9cccf3db5f10301666d9e6d655c970cda5ac23b
Felipe Rotermel <feliperotermell@gmail.com>
2023-08-11 14:37:12 -0300

    compra.py add

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.models import Autor, Categoria, Editora, Livro, Compra
 
 admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
+admin.site.register(Compra)
